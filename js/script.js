212876b349a170535699507a7989a0b14bb23e13
Charles Nguyen <cpnguy@gmail.com>
2017-07-03 21:31:44 -0700

    setup
    setup the basic JS navbar

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function navbarGen() {
+  console.log("run?");
+  document.getElementById("nav").innerHTML =
+    "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+}
