c8b486f0aee8601cf3414019b601b24d49a71405
Charles Nguyen <cpnguy@gmail.com>
2017-07-03 21:46:31 -0700

    created a dynamic navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 function navbarGen() {
-  console.log("run?");
-  document.getElementById("nav").innerHTML =
-    "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+  console.log("PathName  ="+ window.location.pathname);
+
+
+  if(window.location.href.indexOf("index") > -1)  {
+    document.getElementById("nav").innerHTML =
+      "<strong><a href='index.html'>Home</a></strong><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+  }
+
+  if(window.location.href.indexOf("experience") > -1)  {
+    document.getElementById("nav").innerHTML =
+      "<a href='index.html'>Home</a><strong><a href='experience.html'>Experience</a></strong><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+  }
+
+  if(window.location.href.indexOf("blog") > -1)  {
+    document.getElementById("nav").innerHTML =
+      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><strong><a href='blog.html'>Writing</a></strong><a href='contactpage.html'>Contact</a>";
+  }
+
+  if(window.location.href.indexOf("contactpage") > -1)  {
+    document.getElementById("nav").innerHTML =
+      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><strong><a href='contactpage.html'>Contact</a></strong>";
+  }
+
 }
