a9627525222c4222171a23a44fb87fa67b47a724
Charles Nguyen <cpnguy@gmail.com>
2017-07-04 22:56:08 -0700

    added content insertion and edited the nav

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 function navbarGen() {
-  console.log("PathName  ="+ window.location.pathname);
-
 
   if(window.location.href.indexOf("index") > -1)  {
     document.getElementById("nav").innerHTML =
-      "<strong><a href='index.html'>Home</a></strong><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+      "<a id='active' href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
   }
 
   if(window.location.href.indexOf("experience") > -1)  {
     document.getElementById("nav").innerHTML =
-      "<a href='index.html'>Home</a><strong><a href='experience.html'>Experience</a></strong><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
+      "<a href='index.html'>Home</a><a id='active' href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
   }
 
   if(window.location.href.indexOf("blog") > -1)  {
     document.getElementById("nav").innerHTML =
-      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><strong><a href='blog.html'>Writing</a></strong><a href='contactpage.html'>Contact</a>";
+      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a id='active' href='blog.html'>Writing</a><a href='contactpage.html'>Contact</a>";
   }
 
   if(window.location.href.indexOf("contactpage") > -1)  {
     document.getElementById("nav").innerHTML =
-      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><strong><a href='contactpage.html'>Contact</a></strong>";
+      "<a href='index.html'>Home</a><a href='experience.html'>Experience</a><a href='blog.html'>Writing</a><a id='active' href='contactpage.html'>Contact</a>";
   }
 
 }
