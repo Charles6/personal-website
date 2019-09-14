63269bcecc105103c3e77dbc401fbb49ca67c3c6
Charles Nguyen <cpnguy@gmail.com>
2019-09-14 10:50:17 -0700

    phase 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-
-function help() {
-  console.log("You've found the Easter Egg to my site!");
+console.log('Yes, I have a few "easter eggs" here.');
+console.log('Try running the "siteHelp()" function for the following parameters');
+console.log('"lastname"');
+var lastname = "lastname";
+function siteHelp(input) {
+	switch(input) {
+		case "lastname":
+			console.log("I've found it important that people can pronounce my last name as it's often pronounced incorrectly. You can read more about it at the wikipedia page here. https://en.wikipedia.org/wiki/Nguyen#Pronunciation");
+			break;
+		default:
+			console.log("This was not one of the options.");
+	} 
 }
 
-function color(input) {
-  document.getElementsByTagName("h1")[0].style.color = input;
-  document.getElementsByTagName("nav")[0].style.borderColor = input;
-  document.getElementById("active").style.color = input;
-}
